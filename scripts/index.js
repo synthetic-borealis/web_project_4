f9c25cfb5ecc9a43cfaaae3e977daa5514aeda3d
Elhanan Flesch <snip.coloured@gmail.com>
2021-08-23 22:31:20 +0300

    refactor: move initialCards to seperate file

--- scripts/index.js
+++ scripts/index.js
@@ -27,33 +27,6 @@ function updateEditFormContent() {
   inputProfileJob.value = profileJob.textContent;
 }
 
-const initialCards = [
-  {
-    name: "Yosemite Valley",
-    link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
-  },
-  {
-    name: "Lake Louise",
-    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
-  },
-  {
-    name: "Bald Mountains",
-    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
-  },
-  {
-    name: "Latemar",
-    link: "https://code.s3.yandex.net/web-code/latemar.jpg"
-  },
-  {
-    name: "Vanoise National Park",
-    link: "https://code.s3.yandex.net/web-code/vanoise.jpg"
-  },
-  {
-    name: "Lago di Braies",
-    link: "https://code.s3.yandex.net/web-code/lago.jpg"
-  }
-];
-
 function addPlaceCard(name, link) {
   const placeTemplate = document.querySelector("#place-template").content;
   const placeElement = placeTemplate.querySelector(".place").cloneNode(true);
