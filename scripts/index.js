37c3f42c5ff8a8e4cff51eca48d1a0043b18c3d8
Elhanan Flesch <snip.coloured@gmail.com>
2021-08-23 13:26:30 +0300

    feat: add basic places section functionality to script

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
 // Containers
 let editContainer = document.querySelector(".edit-section");
+const placesSection = document.querySelector(".places");
 
 // Forms
 let editForm = document.querySelector(".edit-form");
@@ -38,6 +39,51 @@ function saveProfile(evt) {
 
   editContainer.classList.remove("edit-section_opened");
 }
+
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
+  },
+  {
+    name: "Lake Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
+  },
+  {
+    name: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg"
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg"
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg"
+  }
+];
+
+function addPlaceCard(name, link) {
+  const placeTemplate = document.querySelector("#place-template").content;
+  const placeElement = placeTemplate.querySelector(".place").cloneNode(true);
+
+  const placeImage = placeElement.querySelector(".place__image");
+  placeImage.style.backgroundImage = `url("${link}")`;
+  placeImage.ariaLabel = name;
+
+  placeElement.querySelector(".place__caption").textContent = name;
+  const btnLike = placeElement.querySelector(".place__like-button");
+  btnLike.addEventListener("click", (evt) => evt.target.classList.toggle("place__like-button_active"));
+  const btnDelete = placeElement.querySelector(".place__delete-button");
+  btnDelete.addEventListener("click", () => placeElement.closest(".place").remove());
+
+  placesSection.append(placeElement);
+}
+initialCards.forEach((card) => addPlaceCard(card.name, card.link));
 
 btnEdit.addEventListener("click", toggleEditFormVisibility);
 btnCloseEditForm.addEventListener("click", toggleEditFormVisibility);
