bc01ad8552494e93dd8250db1ec89a7aae3b7add
Elhanan Flesch <snip.coloured@gmail.com>
2021-09-26 14:14:53 +0300

    feat: create Popup class

--- components/Popup.js
+++ components/Popup.js
@@ -1,21 +1,34 @@
 class Popup {
   constructor(popupSelector) {
-    this._container = document.querySelector(popupSelector);
+    this._selector = popupSelector;
+    this._container = document.querySelector(this._selector);
+  }
+
+  _handleEscClose(evt) {
+    if (evt.key === "Escape") {
+      this.close();
+    }
+  }
+
+  _handlePopupClick(evt) {
+    if (
+      evt.target.classList.contains("close-button")
+      || evt.target === evt.currentTarget
+      ) {
+        this.close();
+      }
   }
 
   open() {
-    //
+    this._container.classList.add(`${this._selector}_opened`);
   }
 
   close() {
-    //
-  }
-
-  _handleEscClose() {
-    //
+    this._container.classList.remove(`${this._selector}_opened`);
   }
 
   setEventListeners() {
-    //
+    document.addEventListener("keydown", this._handleEscClose);
+    this._container.addEventListener("click", this._handlePopupClick);
   }
 }
