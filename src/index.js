988de08f17b5f4b8638f58da27cab583d1b49b4e
Elhanan Flesch <snip.coloured@gmail.com>
2021-09-26 20:50:31 +0300

    chore: finish migrating project to webpack

--- src/index.js
+++ src/index.js
@@ -1,6 +1,2 @@
 import "./pages/index.css";
 import "./pages/index.js";
-
-const numbers = [2, 3, 5];
-const doubledNumbers = numbers.map(number => number * 2);
-console.log(doubledNumbers); // 4, 6, 10
